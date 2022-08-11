90ffcec7cd96ae8319051f74a844d3bd77722c2f
nimishgo <nimish.2711@gmail.com>
2022-08-12 01:19:23 +0530

    created func to add marks and tied them to DOM element

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 let board = [];
+let turn = true; // true for "X" and false for "O"
+
+const cells = [...document.querySelectorAll('#cells')];
 
 const renderBoard = (() => {
-    board = [ 'X' ,'O' , 'X',
-              'O' ,'O' , 'O',
-              'X' ,'X' , 'O'];
+    board = [ '' ,'' , '',
+              '' ,'' , '',
+              '' ,'' , ''];
 
     const render = () => {
         board.forEach( (value,index) => {
-                console.log(value,index);
+            cells[index].textContent = value;
         });
-    }
 
+        (turn) ? turn = false : turn = true;
+    };
     return {board , render};
 })(); 
 
-const cells = Array.from(document.querySelectorAll('#cells'));
+const playerTurns = document.getElementById('gameGrid'); 
+
+playerTurns.addEventListener('click' , (e) => {
+    let index = cells.findIndex( (cells) => {
+        // console.log(e.target , cells ,e.target === e.target);
+         return cells === e.target;
+    });    
+    // console.log(index);
+    if(board[index] === '')
+    {
+        (turn) ? board[index] =  "✅":  board[index] = "❎";
+        renderBoard.render();
+    }
+
+    // renderBoard.render();
+});

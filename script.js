55a813dcae77367a8178acd5d379724df95cbcf9
nimishgo <nimish.2711@gmail.com>
2022-08-12 04:39:15 +0530

    Added winning and tieing conditions

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let board = [];
 let turn = true; // true for "X" and false for "O"
+let win = null;
+const winningPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
 const cells = [...document.querySelectorAll('#cells')];
 
@@ -15,7 +26,20 @@ const renderBoard = (() => {
 
         (turn) ? turn = false : turn = true;
     };
-    return {board , render};
+
+    const checkWin = () => {
+        let winner = null;
+    
+        winningPatterns.forEach( (combo, index) => {
+            if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+                winner = board[combo[0]];
+            }
+        });
+    
+        return winner ? winner : board.includes('') ? null : 'Tie';
+    };
+
+    return {board , render , checkWin};
 })(); 
 
 const playerTurns = document.getElementById('gameGrid'); 
@@ -31,6 +55,17 @@ playerTurns.addEventListener('click' , (e) => {
         (turn) ? board[index] =  "✅":  board[index] = "❎";
         renderBoard.render();
     }
+   
+    win = renderBoard.checkWin();
 
-    // renderBoard.render();
+    const displayWin = document.getElementById('player');
+
+    if (win !== null && win !== 'Tie'){
+    
+        displayWin.textContent = `${win} has won the game`; 
+    }
+    else if(win ==='Tie')
+    {
+        displayWin.textContent = `${win}`
+    }
 });
